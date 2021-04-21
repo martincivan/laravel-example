24cd70f8a30ffdf58ee2a26e4fc41bfc4d689167
Martin Civan <martin.civan5@gmail.com>
2021-04-21 22:03:28 +0200

    Migrations WIP

---
+++ event-app/database/migrations/2021_04_21_194632_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->dateTime("valid_from");
+            $table->dateTime("valid_to");
+            $table->string("title");
+            $table->text("content");
+            $table->decimal('gps_lat', 10, 8);
+            $table->decimal('gps_lng', 11, 8);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
